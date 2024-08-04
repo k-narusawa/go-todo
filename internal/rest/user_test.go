b6e0e8c228ec2fe29824625cecc23ac2cf7574fc
narusawakohei <swkei320@gmail.com>
2024-08-04 17:02:18 +0900

    test: 💍 user_hendlerのテスト追加

--- internal/rest/user_test.go
+++ internal/rest/user_test.go
@@ -61,7 +61,7 @@ func TestUserHandler_Get(t *testing.T) {
 	assert.Equal(t, http.StatusOK, rec.Code)
 }
 
-func TestUserHandler_NotFound(t *testing.T) {
+func TestUserHandler_Get_NotFound(t *testing.T) {
 	mockUserService := new(mocks.UserService)
 	mockUserIdStr := "01911bfa-4993-7b11-ae73-ffef34f92d62"
 
@@ -80,5 +80,52 @@ func TestUserHandler_NotFound(t *testing.T) {
 	}
 
 	err := handler.Get(c)
-	assert.Error(t, err)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusNotFound, rec.Code)
+}
+
+func TestUserHandler_GetAll(t *testing.T) {
+	mockUserService := new(mocks.UserService)
+	mockUserService.On("GetAll").Return([]*domain.User{}, nil).Once()
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/users", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	var handler = rest.UserHandler{
+		UserService: mockUserService,
+	}
+
+	err := handler.GetAll(c)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, rec.Code)
+}
+
+func TestUserHandler_Change(t *testing.T) {
+	mockUserService := new(mocks.UserService)
+	mockUserIdStr := "01911bfa-4993-7b11-ae73-ffef34f92d62"
+
+	mockUser := domain.User{
+		UserID: value.OfUserID(mockUserIdStr),
+		Name:   value.NewName("test"),
+	}
+	mockUserService.On("Change", mock.Anything).Return(&mockUser, nil).Once()
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPut, "/users/"+mockUserIdStr, strings.NewReader(`{"name":"test"}`))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/users/:user_id")
+	c.SetParamNames("user_id")
+	c.SetParamValues(mockUserIdStr)
+
+	var handler = rest.UserHandler{
+		UserService: mockUserService,
+	}
+
+	err := handler.Change(c)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusOK, rec.Code)
 }
