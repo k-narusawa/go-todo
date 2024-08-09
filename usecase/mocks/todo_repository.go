1b1914a2ae892ea7eb20ff1d587fcb71f7258500
narusawakohei <swkei320@gmail.com>
2024-08-10 08:49:04 +0900

    refactor: 💡 不要なコメントを削除

--- usecase/mocks/todo_repository.go
+++ usecase/mocks/todo_repository.go
@@ -11,15 +11,6 @@ type ToDoRepository struct {
 	mock.Mock
 }
 
-// type ToDoRepository interface {
-// 	FindAll() ([]domain.ToDo, error)
-// 	FindByID(todoId value.ToDoID) (*domain.ToDo, error)
-// 	FindByUserID(userId value.UserID) ([]domain.ToDo, error)
-// 	Store(todo domain.ToDo) error
-// 	Update(todo domain.ToDo) error
-// 	Delete(todoId value.ToDoID) error
-// }
-
 func (m *ToDoRepository) FindAll() ([]domain.ToDo, error) {
 	ret := m.Called()
 
